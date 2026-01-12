3cc6c1f618276d76f99b6b66199b0fb3b098cf3e
khangtruong-ui <khang.truong@gmail.com>
2026-01-12 20:01:48 +0700

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import flax.linen as nn
 
 from config import *
 
-
+# =========================== UTILITIES ====================================
 class Mask(nn.Module):
     def __call__(self, inputs):
         length = inputs.shape[-2]
@@ -21,7 +21,7 @@ class SeqEmbedding(nn.Module):
     def __call__(self, txt):
         return self.token_emb(txt) + self.position(jnp.arange(txt.shape[-1]))[None, ...]
 
-
+# ================================ ATTENTION THINGS ====================================
 class MemorizedAttention(nn.Module):
     dim: int
     def setup(self):
@@ -40,6 +40,50 @@ class MemorizedAttention(nn.Module):
         attn = jax.nn.softmax(attn, axis=-1)
         return jnp.einsum('...qk, ...kd -> ...qd', attn, V)
 
+class MultiheadMemorizedAttention(nn.Module):
+    def setup(self):
+        self.heads = [MemorizedAttention(TEXT_EMBEDDING_DIM // ATTENTION_HEAD) for _ in range(ATTENTION_HEAD)]
+        self.norm = nn.LayerNorm()
+        self.dense = nn.Dense(TEXT_EMBEDDING_DIM)
+
+    def __call__(self, q, k, v):
+        tensors = [head(q, k, v) for head in self.heads]
+        return self.norm(self.dense(jnp.concatenate(tensors, axis=-1)) + q)
+
+class MultiheadAttention(nn.Module):
+    def setup(self):
+        self.attn = nn.MultiHeadDotProductAttention(
+            num_heads=ATTENTION_HEAD, 
+            qkv_features=TEXT_EMBEDDING_DIM,
+            out_features=TEXT_EMBEDDING_DIM,
+        )
+
+    def __call__(self, q, k, v):
+        return self.attn(q, k, v)
+
+class MultiheadStaticAttention(nn.Module):
+    def setup(self):
+        self.Q = nn.Dense(TEXT_EMBEDDING_DIM)
+        self.V = nn.Dense(TEXT_EMBEDDING_DIM)
+        self.norm = nn.LayerNorm(TEXT_EMBEDDING_DIM)
+        self.memory = self.param('memory', 
+                                 nn.initializers.glorot_uniform(), 
+                                 (EXPANSION_LENGTH, ATTENTION_HEAD, TEXT_EMBEDDING_DIM // ATTENTION_HEAD))
+
+    def __call__(self, q, k, v):
+        batch = q.shape[0]
+        length = q.shape[2]
+        dim = q.shape[3]
+        Q_ = self.Q(q)
+        V_ = self.V(k)
+        Q = Q_.reshape(batch, length, ATTENTION_HEAD, dim // ATTENTION_HEAD)
+        V = V_.reshape(batch, length, ATTENTION_HEAD, dim // ATTENTION_HEAD)
+        eij = jnp.einsum('ehd, blhd -> belh', self.memory, Q)
+        eij = jnp.where(eij > 0, eij, 0.)
+        out1 = jnp.einsum('belh, blhd -> behd', eij, V)
+        out2 = jnp.einsum('behd, belh -> blhd', out1, eij)
+        out = self.norm(out2.reshape(batch, 1, length, dim))
+        
 
 
 
