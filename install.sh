cb6a230fb748234cdfff359560118ba57c0f88f7
khangtruong-ui <khang.truong@gmail.com>
2026-01-12 11:38:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-pip install -q datasets einops
+pip install -q jax[tpu] flax optax datasets Pillow matplotlib grain -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 gcloud storage cp -r gs://khang-nwpu/ds .
