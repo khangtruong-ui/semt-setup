f7707379465faa37104576c9ec4725212ea1c5b0
khangtruong-ui <khang.truong@gmail.com>
2026-01-12 15:57:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,29 @@ ATTENTION_HEAD = 16
 ATTENTION_CHOICE = 3
 DECODER_ATTENTION_CHOICE = 2
 BACKBONE_CHOICE = 3
+
+model_name = {
+    0: 'RVSA',
+    1: 'Meshed_memory',
+    2: 'Zero_mesh',
+    3: 'Static_attention',
+    4: 'Captured_static_attention',
+}[ATTENTION_CHOICE]
+
+decoder_model = {
+    0: 'Mesh_decoder',
+    1: 'T5',
+    2: 'No_mesh',
+    3: 'Dynamic_attention',
+}[DECODER_ATTENTION_CHOICE]
+
+vision_name = {
+    0: 'Resnet152',
+    1: 'Resnet50',
+    2: 'VGG16',
+    3: 'EfficientNetB2',
+    4: 'Short',
+    5: 'Swin',
+    6: 'Inception',
+    7: 'MobileNet'
+}[BACKBONE_CHOICE]
