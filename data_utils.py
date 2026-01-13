51ab5138d4e2cb171857e4c91fe01f16b4cd08f8
khangtruong-ui <khang.truong@gmail.com>
2026-01-13 20:34:15 +0700

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -54,7 +54,7 @@ def get_set(ds):
             drop_remainder=True,
         ),
         shuffle=True,
-        seed=jax.random.key(0),
+        seed=42,
     )
     
     if not os.path.exists('tokenizer.json'):
