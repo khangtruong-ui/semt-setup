07547a05266d5d53385a1404e471d058f8ab7e52
khangtruong-ui <khang.truong@gmail.com>
2026-01-13 15:11:37 +0700

    Update train.py

--- train.py
+++ train.py
@@ -6,8 +6,10 @@ from model import MeshedFastCaption
 from train_util import *
 from data_utils import get_train_set
 
-ds = get_train_set()
 
 model = MeshedFastCaption()
 state = create_train_state(model)
+
+
+ds = get_train_set()
 state = train_loop(state, ds, 1)
