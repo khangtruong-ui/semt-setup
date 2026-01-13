5e117d4968071d5551b9eb320ccfa51d4bb2af6d
khangtruong-ui <khang.truong@gmail.com>
2026-01-13 20:28:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-pip install -q jax[tpu] flax optax datasets Pillow matplotlib einops eqxvision equinox grain -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install -q jax[tpu] flax optax datasets Pillow matplotlib einops grain -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 gcloud storage cp -r $DATASET .
