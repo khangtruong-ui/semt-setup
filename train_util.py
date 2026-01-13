530b9e48154baf0fa3cdb133e0391ef9239bd780
khangtruong-ui <khang.truong@gmail.com>
2026-01-13 21:06:26 +0700

    Update train_util.py

--- train_util.py
+++ train_util.py
@@ -55,7 +55,6 @@ def train_loop(model, train_state, ds, epoches):
         with tqdm(total=ds_length, desc=f"Epoch {epoch}") as pbar:
             running_loss = 0.
             for i, item in zip(range(ds_length), iter_ds):
-                print(item)
                 image = jax.device_put(item['image'], sharding)
                 pad_left = jax.device_put(item['pad_left_ids'], sharding)
                 pad_right = jax.device_put(item['pad_right_ids'], sharding)
