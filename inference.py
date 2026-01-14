1771efff76dc359717a5f08010f0ec58835e9afc
khangtruong-ui <khang.truong@gmail.com>
2026-01-14 14:41:13 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -11,6 +11,10 @@ import json
 from data_utils import get_train_set, load_tokenizer
 from model import MeshedFastCaption
 
+mesh = Mesh(np.array(jax.devices()), ('data',))
+sharding = NamedSharding(mesh, P('data'))
+non_sharding = no_sharding = NamedSharding(mesh, P())
+
 
 def load_checkpoint():
     fname = sorted(glob.glob('*.msgpack'))[-1]
@@ -30,15 +34,15 @@ def inference(model, weights):
     test_set = get_train_set()
 
     @jax.jit
-    def loop_body(batch):
-        image = batch['image']
+    def loop_body(image):
         out = model.apply(weights, image, method=model.batch_generate_caption)
         return out
 
     ret_dict = []
     res_dict = []
     for _, batch in zip(range(100), test_set):
-        out = loop_body(batch)
+        image = jax.device_put(batch['image'], sharding)
+        out = loop_body(image)
         out_sentence = reverse_tensor(out)
         out_caption = reverse_tensor(batch['pad_right_ids'])
         print(out_caption)
