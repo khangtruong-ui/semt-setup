4ad0502c527b3894373a76bdada6fdd4cb8f54f6
khangtruong-ui <khang.truong@gmail.com>
2026-01-14 16:19:07 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -53,8 +53,8 @@ def inference(model, weights):
         out_caption = reverse_tensor(batch['pad_right_ids'])
         print(out_caption)
         print(out_sentence)
-        ret_dict.append(out_sentence)
-        res_dict.append(out_caption)
+        ret_dict.append(out_sentence.tolist())
+        res_dict.append(out_caption.tolist())
 
     with open('predict.json', 'w') as f:
         json.dump(ret_dict, f)
