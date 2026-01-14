796da1993634f3588dcbfceb4788028d44a50047
khangtruong-ui <khang.truong@gmail.com>
2026-01-14 11:38:41 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,17 @@
-
+import optax
+import jax
+import jax.numpy as jnp
+from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
+import numpy as np
+from tqdm import tqdm
+from flax import serialization
+import glob
 
 
 def load_checkpoint():
+    fname = sorted(glob.glob('*.msgpack'))[-1]
     with open('weights.msgpack', 'rb') as f:
         return serialization.from_bytes(f.read())
+
+def inference(model, weights):
+    pass
