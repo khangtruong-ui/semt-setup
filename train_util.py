13abe6afc6b1012b8096e2afcd48c287cbb62e1e
khangtruong-ui <khang.truong@gmail.com>
2026-01-15 17:13:32 +0700

    Update train_util.py

--- train_util.py
+++ train_util.py
@@ -63,9 +63,10 @@ def train_loop(model, train_state, ds, epoches):
 
         return train_state
 
+    iter_ds = iter(ds)
     with ThreadPoolExecutor(max_workers=2) as executor:
         for epoch in range(epoches):
-            train_state = train_epoch(epoch, train_state, ds)
+            train_state = train_epoch(epoch, train_state, iter_ds)
             if (epoch + 1) % 1 == 0:
                 executor.submit(save_checkpoint, train_state.params, epoch)
 
