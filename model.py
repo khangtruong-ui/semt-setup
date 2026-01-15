d71267cc886485b87240e8bb141956df9f704604
khangtruong-ui <khang.truong@gmail.com>
2026-01-15 11:51:35 +0700

    Update model.py

--- model.py
+++ model.py
@@ -126,9 +126,9 @@ class ShortVision(nn.Module):
     def __call__(self, x):
         x = self.preprocessing(x)
         out = x
-        out = nn.Conv(32, (2, 2), strides=(2, 2))(out)
-        out = nn.Conv(64, (2, 2), strides=(2, 2))(out)
-        out = nn.Conv(TEXT_EMBEDDING_DIM, (2, 2), strides=(2, 2))(out)
+        out = nn.Conv(32, (2, 2), strides=(4, 4))(out)
+        out = nn.Conv(64, (2, 2), strides=(4, 4))(out)
+        out = nn.Conv(128, (2, 2), strides=(2, 2))(out)
         return out
 
 # ==================================== ENCODERS DECODERS =============================
