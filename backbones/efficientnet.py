a91891b16a7d78a1b6ef223471f0c5dcd7beb775
khangtruong-ui <khang.truong@gmail.com>
2026-01-17 14:41:16 +0700

    Update efficientnet.py

--- backbones/efficientnet.py
+++ backbones/efficientnet.py
@@ -125,7 +125,6 @@ class EfficientNetB1(linen.Module):
 
   def forward(self, img):
     weights = self.pretrained_weights()
-    print(jax.tree.map(jnp.shape, weights))
     return self.apply(weights, img, train=False, mutable=False)
     
   def pretrained_weights(self):
