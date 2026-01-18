ddf53c06d72014153fb27907bd7af76fdcce23e4
khangtruong-ui <khang.truong@gmail.com>
2026-01-18 22:27:37 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -49,7 +49,7 @@ def inference(model, weights):
 
     ret_dict = []
     res_dict = []
-    for _, batch in zip(range(1), test_set):
+    for _, batch in zip(range(2), test_set):
         batch = jax.tree.map(np.array, batch)
         image = jax.device_put(batch['image'], sharding)
         out = loop_body(image)
