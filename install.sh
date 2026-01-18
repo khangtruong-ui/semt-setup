00186a74f8a7a77b27a1f196e44dd7fee958d581
khangtruong-ui <khang.truong@gmail.com>
2026-01-18 22:43:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-pip install -q jax[tpu] flax optax datasets transformers Pillow matplotlib tensorflow keras einops grain rouge-score nltk torch -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install -q jax[tpu] flax optax datasets transformers Pillow matplotlib tensorflow keras einops grain nltk torch -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
