b38645aa54eefc86190597e9dad9de241a1e7f32
khangtruong-ui <khang.truong@gmail.com>
2026-01-18 13:45:42 +0700

    Update train.py

--- train.py
+++ train.py
@@ -7,11 +7,11 @@ from train_util import *
 from data_utils import get_train_set
 
 print('===== CRAFTING DATASETS =====')
-ds = get_train_set()
+ds, ds_length = get_train_set()
 
 print('====== CRAFTING MODELS ======')
 model = MeshedFastCaption()
 state = create_train_state(model)
 
 print('===== TRAINING ======')
-state = train_loop(model, state, ds, 20)
+state = train_loop(model, state, ds, ds_length, 20)
